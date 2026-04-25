9d2101c5d3e43df9875947cbb8a3e3f8c1c830f5
davidkosvica <kosvica@1webit.cz>
2026-04-25 20:48:12 +0200

    basic interaction with api

--- src/components/DrawCardButton/DrawCardButton.tsx
+++ src/components/DrawCardButton/DrawCardButton.tsx
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './DrawCardButton.module.css';
 
+interface ShuffledDeckResponse {
+    success: boolean;
+    deck_id: string;
+    shuffled: boolean;
+    remaining: number;
+}
+
 export default function DrawCardButton(): React.ReactElement {
-  return (
-    <div className={styles.drawCardButtonContainer}>
-        <button className={styles.drawCardButton}>
-        Draw card
-        </button>
-    </div>
-  );
+    const [deck, setResponse] = useState<ShuffledDeckResponse | null>(null);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [isInitialized, setIsInitialized] = useState(false);
+
+    function drawCard(): void
+    {
+         if (isSubmitting) return;
+
+         if (!isInitialized) {
+            initShuffledDeckOfCards();
+         }
+
+
+    }
+
+    async function initShuffledDeckOfCards(): Promise<ShuffledDeckResponse> {
+       
+
+        const url = `${import.meta.env.VITE_API_BASE_URL}/api/deck/new/shuffle/?deck_count=1`;
+
+        setIsSubmitting(true);
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch card: ${response.status} ${response.statusText}`);
+        }
+
+        setIsSubmitting(false);
+        
+
+        const deck: ShuffledDeckResponse = JSON.parse(await response.text());
+
+        setResponse(deck);
+        setIsInitialized(true);
+
+        return deck;
+    }
+
+    return (
+        <div className={styles.drawCardButtonContainer} onClick={drawCard}>
+            <button disabled={isSubmitting} className={styles.drawCardButton}>
+            Draw card
+            </button>
+        </div>
+    );
 }
